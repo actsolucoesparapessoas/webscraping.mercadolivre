df543663cdb9a42c76827f58fb62300a5e8c2813
actsolucoesparapessoas <129021588+actsolucoesparapessoas@users.noreply.github.com>
2024-06-19 01:19:48 -0300

    Update raspa_mercado_livre.py

--- raspa_mercado_livre.py
+++ raspa_mercado_livre.py
@@ -205,13 +205,13 @@ with tab1:
 
         with col2:
             st.write(''' ## **Dicas de uso:** ''')
-            st.write(''' ##### **1ºPasso:** Digite a QTD e 3 Palavras chave na Descrição''')          
+            st.write(''' ##### **1º)** Digite a QTD e 3 Palavras chave na Descrição''')          
             Passo2 = '<p style="font-weight: bolder; color:red; font-size: 18px">Cadastrar na Lista de Compras</p>'
             Passo3 = '<p style="font-weight: bolder; color:darkgray; font-size: 16px">Pesquisar a Lista de Compras Cadastrada</p>'            
             
-            st.write(''' ##### **2ºPasso:** Clique no botão''') 
+            st.write(''' ##### **2º)** Clique no botão''') 
             st.markdown(Passo2, unsafe_allow_html=True)
-            st.write(''' ##### **3ºPasso:** Clique no botão''')  
+            st.write(''' ##### **3º)** Clique no botão''')  
             st.markdown(Passo3, unsafe_allow_html=True)
             mystyle2 =   '''<style> p{text-align:justified;}</style>'''
             st.markdown(mystyle2, unsafe_allow_html=True)      
